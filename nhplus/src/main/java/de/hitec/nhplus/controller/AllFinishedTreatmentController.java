f5da9d6603bc82d21bb9fa159afaeb59b73dc1a1
Michael Huse <michael.huse@wolters-rundreisen.de>
2025-06-03 13:08:14 +0200

    Added a button to view finished treatments and alle the logic for it.

---
+++ nhplus/src/main/java/de/hitec/nhplus/controller/AllFinishedTreatmentController.java
@@ -0,0 +1,180 @@
+package de.hitec.nhplus.controller;
+
+import de.hitec.nhplus.Main;
+import de.hitec.nhplus.datastorage.DaoFactory;
+import de.hitec.nhplus.datastorage.PatientDao;
+import de.hitec.nhplus.datastorage.FinishedTreatmentDao;
+import de.hitec.nhplus.model.Patient;
+import de.hitec.nhplus.model.Treatment;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class AllFinishedTreatmentController {
+
+    @FXML
+    private TableView<Treatment> tableView;
+
+    @FXML
+    private TableColumn<Treatment, Integer> columnId;
+
+    @FXML
+    private TableColumn<Treatment, Integer> columnPid;
+
+    @FXML
+    private TableColumn<Treatment, String> columnDate;
+
+    @FXML
+    private TableColumn<Treatment, String> columnBegin;
+
+    @FXML
+    private TableColumn<Treatment, String> columnEnd;
+
+    @FXML
+    private TableColumn<Treatment, String> columnDescription;
+
+    @FXML
+    private ComboBox<String> comboBoxPatientSelection;
+
+    @FXML
+    private Button buttonDelete;
+
+    private final ObservableList<Treatment> finishedTreatments = FXCollections.observableArrayList();
+    private FinishedTreatmentDao dao;
+    private final ObservableList<String> patientSelection = FXCollections.observableArrayList();
+    private ArrayList<Patient> patientList;
+
+    public void initialize() {
+        readAllAndShowInTableView();
+        comboBoxPatientSelection.setItems(patientSelection);
+        comboBoxPatientSelection.getSelectionModel().select(0);
+
+        this.columnId.setCellValueFactory(new PropertyValueFactory<>("tid"));
+        this.columnPid.setCellValueFactory(new PropertyValueFactory<>("pid"));
+        this.columnDate.setCellValueFactory(new PropertyValueFactory<>("date"));
+        this.columnBegin.setCellValueFactory(new PropertyValueFactory<>("begin"));
+        this.columnEnd.setCellValueFactory(new PropertyValueFactory<>("end"));
+        this.columnDescription.setCellValueFactory(new PropertyValueFactory<>("description"));
+        this.tableView.setItems(this.finishedTreatments);
+
+        // Disabling the button to delete finishedTreatments as long, as no treatment was selected.
+        this.buttonDelete.setDisable(true);
+        this.tableView.getSelectionModel().selectedItemProperty().addListener(
+                (observableValue, oldTreatment, newTreatment) ->
+                        AllFinishedTreatmentController.this.buttonDelete.setDisable(newTreatment == null));
+
+        this.createComboBoxData();
+    }
+
+    public void readAllAndShowInTableView() {
+        this.finishedTreatments.clear();
+        comboBoxPatientSelection.getSelectionModel().select(0);
+        this.dao = DaoFactory.getDaoFactory().createFinishedTreatmentDao();
+        try {
+            this.finishedTreatments.addAll(dao.readAll());
+        } catch (SQLException exception) {
+            exception.printStackTrace();
+        }
+    }
+
+    private void createComboBoxData() {
+        PatientDao dao = DaoFactory.getDaoFactory().createPatientDAO();
+        try {
+            patientList = (ArrayList<Patient>) dao.readAll();
+            this.patientSelection.add("alle");
+            for (Patient patient: patientList) {
+                this.patientSelection.add(patient.getSurname());
+            }
+        } catch (SQLException exception) {
+            exception.printStackTrace();
+        }
+    }
+
+
+    @FXML
+    public void handleComboBox() {
+        String selectedPatient = this.comboBoxPatientSelection.getSelectionModel().getSelectedItem();
+        this.finishedTreatments.clear();
+        this.dao = DaoFactory.getDaoFactory().createFinishedTreatmentDao();
+
+        if (selectedPatient.equals("alle")) {
+            try {
+                this.finishedTreatments.addAll(this.dao.readAll());
+            } catch (SQLException exception) {
+                exception.printStackTrace();
+            }
+        }
+
+        Patient patient = searchInList(selectedPatient);
+        if (patient !=null) {
+            try {
+                this.finishedTreatments.addAll(this.dao.readFinishedTreatmentsByPid(patient.getPid()));
+            } catch (SQLException exception) {
+                exception.printStackTrace();
+            }
+        }
+    }
+
+    private Patient searchInList(String surname) {
+        for (Patient patient : this.patientList) {
+            if (patient.getSurname().equals(surname)) {
+                return patient;
+            }
+        }
+        return null;
+    }
+
+    @FXML
+    public void handleDelete() {
+        int index = this.tableView.getSelectionModel().getSelectedIndex();
+        Treatment t = this.finishedTreatments.remove(index);
+        FinishedTreatmentDao dao = DaoFactory.getDaoFactory().createFinishedTreatmentDao();
+        try {
+            dao.deleteById(t.getTid());
+        } catch (SQLException exception) {
+            exception.printStackTrace();
+        }
+    }
+
+
+    @FXML
+    public void handleMouseClick() {
+        tableView.setOnMouseClicked(event -> {
+            if (event.getClickCount() == 2 && (tableView.getSelectionModel().getSelectedItem() != null)) {
+                int index = this.tableView.getSelectionModel().getSelectedIndex();
+                Treatment finishedTreatments = this.finishedTreatments.get(index);
+                treatmentWindow(finishedTreatments);
+            }
+        });
+    }
+
+
+    public void treatmentWindow(Treatment treatment){
+        try {
+            FXMLLoader loader = new FXMLLoader(Main.class.getResource("/de/hitec/nhplus/FinishedTreatmentView.fxml"));
+            AnchorPane pane = loader.load();
+            Scene scene = new Scene(pane);
+
+            // the primary stage should stay in the background
+            Stage stage = new Stage();
+            FinishedTreatmentController controller = loader.getController();
+            controller.initializeController(this, stage, treatment);
+
+            stage.setScene(scene);
+            stage.setResizable(false);
+            stage.showAndWait();
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
+    }
+}
